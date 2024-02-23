3d7182bc2c7c28077b391fcb79b5674f95418a8c
DorisAdriana <doriswezenberg@live.nl>
2024-02-23 16:51:32 +0100

    all files processed correctly and ready for importing them

--- access_images.py
+++ access_images.py
@@ -69,74 +69,136 @@ import os
 # only 34 with the same shape, but as we will be processing in 4D will have more data
 # then 2380 image slices in the folder 4dflow
 
-### SET VAR
-finalfolders = ['TOW037', 'TOW106', 'TOW068', 'TOW033', 'TOW_VOL03', 'TOW_VOL01', 'TOW224', 'TOW048', 'TOW216', 'TOW_VOL04', 'TOW013', 'TOW254', 'TOW_VOL07',
-                 'TOW_VOL09', 'TOW132', 'TOW125', 'TOW049', 'TOW_VOL10', 'TOW259', 'TOW080', 'TOW257', 'TOW042', 'TOW_VOL002', 'TOW113', 'TOW097', 'TOW040', 
-                 'TOW251', 'TOW046', 'TOW_VOL05', 'TOW201', 'TOW_VOL06', 'TOW017', 'TOW011', 'TOW018']
 
-
-
+###### OLD ######
+# finalfolders = ['TOW037', 'TOW106', 'TOW068', 'TOW033', 'TOW_VOL03', 'TOW_VOL01', 'TOW224', 'TOW048', 'TOW216', 'TOW_VOL04', 'TOW013', 'TOW254', 'TOW_VOL07',
+#                  'TOW_VOL09', 'TOW132', 'TOW125', 'TOW049', 'TOW_VOL10', 'TOW259', 'TOW080', 'TOW257', 'TOW042', 'TOW_VOL002', 'TOW113', 'TOW097', 'TOW040', 
+#                  'TOW251', 'TOW046', 'TOW_VOL05', 'TOW201', 'TOW_VOL06', 'TOW017', 'TOW011', 'TOW018']
 # deze precies zo processen maar storen in processed map als jpeg. Dan kunnen trainen :)
 # dit worden dus alle 34 met 256x256 shape, proberen zo te runnen en alle 64 in configs file naar 256 doen en proberen, anders reshapen
 from PIL import Image
 import re
+import numpy as np
 
-# outputdir = 'my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/Data/processed/4dflow'
+
+
+# ###### 4DFLOW ######
 # path = "my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/Data/4dflow"
+# outputdir = 'my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/Data/processed/4dflow'
 
-### CREATE 4d flow data
-# small check, now 35701 files, sholuld be 35700 so check what is the extra file
+# finalfolders = ['TOW037', 'TOW106', 'TOW068', 'TOW033', 'TOW_VOL03', 'TOW_VOL01', 'TOW224', 'TOW048', 'TOW216', 'TOW_VOL04', 'TOW013', 'TOW254', 'TOW_VOL07',
+#                  'TOW_VOL09', 'TOW132', 'TOW125', 'TOW049', 'TOW_VOL10', 'TOW259', 'TOW080', 'TOW257', 'TOW042', 'TOW_VOL002', 'TOW113', 'TOW097', 'TOW040', 
+#                  'TOW251', 'TOW046', 'TOW_VOL05', 'TOW201', 'TOW_VOL06', 'TOW017', 'TOW011', 'TOW018']
 # pattern = re.compile(r'TOW(?:_VOL)?(\d+)_')
 # for folder in finalfolders:
 #     for file in os.listdir(path+"/"+folder):
 #         if "rec-mag" in file:
 #             file4d = file
 #             img = nib.load(path+"/"+folder+"/"+file4d).get_fdata()
-#             resized_img_1 = img[:,:,:,::2]
-#             resized_img_2 = resized_img_1[:,:,:,::2]
-#             # print(file4d)
-#             # print(resized_img_2.shape)
-#             # print(type(resized_img_2))
+#             img = img[:,:,:,::2]
+#             img_arr = np.array(img)
+#             norm_img_arr = 255 * (img_arr - np.min(img_arr)) / (np.max(img_arr) - np.min(img_arr))
+#             norm_img_arr = norm_img_arr.astype(np.uint8)
+#             img = norm_img_arr
+#             # img = img[:,:,:,0]
+# #           print(file4d)
+# #           print(img.shape)
+# #           print(type(resized_img_2))
 #             match = pattern.search(file4d)
-#             for i in range(resized_img_2.shape[2]):
-#                 for j in range(resized_img_2.shape[3]):
-#                     slice_2d = resized_img_2[:,:,i,j]
+#             for i in range(img.shape[2]):
+#                 for j in range(img.shape[3]):
+#                     slice_2d = img[:,:,i,j]
 #                     image = Image.fromarray(slice_2d)
+#                     # print(image.mode)
+
 #                     if image.mode != 'RGB':
 #                         image = image.convert('RGB')
 #                     filename = f'img_{match.group(0)}_slice_{i+1}_{j+1}.jpg'
 #                     file_path = os.path.join(outputdir, filename)
-#                     # print(file4d, slice_2d.shape, i, j, filename)
+#                     print(file4d, slice_2d.shape, i, j, filename)
 #                     image.save(file_path, 'JPEG')
 
+
+
+# ###### bSSFP ######
+# print("starting")
+# path = "my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/Data/3d_cine"
+# outputdir = 'my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/Data/processed/bSSFP'
+
+# finalfolders = ['TOW037', 'TOW106', 'TOW068', 'TOW033', 'TOW_VOL03', 'TOW_VOL01', 'TOW224', 'TOW048', 'TOW216', 'TOW_VOL04', 'TOW013', 'TOW254', 'TOW_VOL07',
+#                  'TOW_VOL09', 'TOW132', 'TOW125', 'TOW049', 'TOW_VOL10', 'TOW259', 'TOW080', 'TOW257', 'TOW042', 'TOW_VOL002', 'TOW113', 'TOW097', 'TOW040', 
+#                  'TOW251', 'TOW046', 'TOW_VOL05', 'TOW201', 'TOW_VOL06', 'TOW017', 'TOW011', 'TOW018']
+# pattern = re.compile(r'TOW(?:_VOL)?(\d+)_')
+
+# for folder in finalfolders:
+#     for file in os.listdir(path+"/"+folder):
+#         file4d = file
+#         img = nib.load(path+"/"+folder+"/"+file4d).get_fdata()
+#         # img = img[:,:,:,::2]
+#         img_arr = np.array(img)
+#         norm_img_arr = 255 * (img_arr - np.min(img_arr)) / (np.max(img_arr) - np.min(img_arr))
+#         norm_img_arr = norm_img_arr.astype(np.uint8)
+#         img = norm_img_arr
+#         # img = img[:,:,:,0]
+# #           print(file4d)
+# #           print(img.shape)
+# #           print(type(resized_img_2))
+#         match = pattern.search(file4d)
+#         for i in range(img.shape[2]):
+#             for j in range(img.shape[3]):
+#                 slice_2d = img[:,:,i,j]
+#                 image = Image.fromarray(slice_2d)
+#                 # print(image.mode)
+
+#                 if image.mode != 'RGB':
+#                     image = image.convert('RGB')
+#                 filename = f'img_{match.group(0)}_slice_{i+1}_{j+1}.jpg'
+#                 file_path = os.path.join(outputdir, filename)
+#                 print(file4d, slice_2d.shape, i, j, filename)
+#                 image.save(file_path, 'JPEG')
+
+
 # print("Finished")
-# "my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/Data/4dflow"
-path = "my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/Data/3d_cine"
-outputdir = 'my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/Data/processed/bSSFP'
 
-filecount = 0
-pattern = re.compile(r'TOW(?:_VOL)?(\d+)_')
 
-for folder in finalfolders:
-    for file in os.listdir(path+"/"+folder):
-        # print(file)
-        # filecount+=1
-        bssfp = file
-        match = pattern.search(bssfp)
-        img = nib.load(path+"/"+folder+"/"+bssfp).get_fdata()
-        # print(file4d)
-        # print(img.shape)
-        # print(type(resized_img))
-        for i in range(img.shape[2]):
-            for j in range(img.shape[3]):
-                slice_2d = img[:,:,i,j]
-                image = Image.fromarray(slice_2d)
-                if image.mode != 'RGB':
-                    image = image.convert('RGB')
-                filename = f'img_{match.group(0)}_slice_{i+1}_{j+1}.jpg'
-                file_path = os.path.join(outputdir, filename)
-                # print(file4d, slice_2d.shape, i, j, filename)
-                image.save(file_path, 'JPEG')
+###### Folders in right location ######
+## Adapt folder name and path and use this to store data in the right folders
+### SET VAR
+finalfolders = ['TOW037', 'TOW106', 'TOW068', 'TOW033', 'TOW_VOL03', 'TOW_VOL01', 'TOW224', 'TOW048', 'TOW216', 'TOW_VOL04', 'TOW013', 'TOW254', 'TOW_VOL07',
+                 'TOW_VOL09', 'TOW132', 'TOW125', 'TOW049', 'TOW_VOL10', 'TOW259', 'TOW080', 'TOW257', 'TOW042', 'TOW_VOL002', 'TOW113', 'TOW097', 'TOW040', 
+                 'TOW251', 'TOW046', 'TOW_VOL05', 'TOW201', 'TOW_VOL06', 'TOW017', 'TOW011', 'TOW018']
 
-print("Finished")
+trainfolders = ['TOW037', 'TOW106', 'TOW068', 'TOW033', 'TOW_VOL03', 'TOW_VOL01', 'TOW224', 'TOW048', 'TOW216', 'TOW_VOL04', 'TOW013', 'TOW254', 'TOW_VOL07',
+                 'TOW_VOL09', 'TOW132', 'TOW125', 'TOW049', 'TOW_VOL10', 'TOW259', 'TOW080']
+testfolders = ['TOW257', 'TOW042', 'TOW_VOL002', 'TOW113', 'TOW097', 'TOW040', 
+                 'TOW251']
+valfolders = ['TOW046', 'TOW_VOL05', 'TOW201', 'TOW_VOL06', 'TOW017', 'TOW011', 'TOW018']
+
+import os
+import shutil
+
+# Define the source folders and the destination folders
+source_folders = {'4dflow_15ts': 'my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/Data/processed/4dflow_15ts', 'bSSFP': 'my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/Data/processed/bSSFP'}
+destination_folders = {'4dflow_15ts': 'my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/BBDM_Thesis/data/processed/val/A',
+                       'bSSFP': 'my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/BBDM_Thesis/data/processed/val/B'}
+
+# Function to copy files for the first 20 scans
+def copy_files_for_scans(source, destination):
+    # Loop through the first 20 scans
+    for scan in valfolders: 
+        # For each scan, loop through the slices
+        for N in range(1, 71):
+            for M in range(1, 16):
+                file_name = f'img_{scan}__slice_{N}_{M}.jpg'
+                source_file = os.path.join(source, file_name)
+                destination_file = os.path.join(destination, file_name)
+                # Copy file if it exists
+                if os.path.exists(source_file):
+                    shutil.copy(source_file, destination_file)
+                else:
+                    print(f"File does not exist: {source_file}")
+
+# Run the copy function for each folder
+for key in source_folders:
+    copy_files_for_scans(source_folders[key], destination_folders[key])
 
