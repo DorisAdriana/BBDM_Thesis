3e94b0951b454282d884dd24e7ff26d69e5f4ef2
DorisAdriana <doriswezenberg@live.nl>
2024-03-06 16:03:51 +0100

    processing all sizes

--- doris_utils/access_images.py
+++ doris_utils/access_images.py
@@ -3,7 +3,7 @@ import matplotlib.pylab as plt
 import os
 
 # print(os.getcwd())
-# print(os.listdir("/home/rnga/dawezenberg/my-rdisk/r-divi/RNG/Temp/dawezenberg"))
+# print(os.listdir("/home/rnga/dawezenberg/my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/BBDM_Thesis"))
 # files4dflow = os.listdir("/home/rnga/dawezenberg/my-rdisk/r-divi/RNG/Temp/dawezenberg/Data/4dflow")
 # files3dcine = os.listdir("/home/rnga/dawezenberg/my-rdisk/r-divi/RNG/Temp/dawezenberg/Data/3d_cine")
 # print("4dflow",files4dflow)
@@ -11,7 +11,7 @@ import os
 
 # for element in files4dflow:
 #     if element not in files3dcine:
-#         print(element)
+#         print("not in", element)
 
 
 # for element in files3dcine:
@@ -83,40 +83,46 @@ import numpy as np
 
 
 # ###### 4DFLOW ######
-# path = "my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/Data/4dflow"
+
+finalfolders = os.listdir("/home/rnga/dawezenberg/my-rdisk/r-divi/RNG/Temp/dawezenberg/Data/4dflow")
+path = "my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/Data/4dflow"
 # outputdir = 'my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/Data/processed/4dflow'
 
 # finalfolders = ['TOW037', 'TOW106', 'TOW068', 'TOW033', 'TOW_VOL03', 'TOW_VOL01', 'TOW224', 'TOW048', 'TOW216', 'TOW_VOL04', 'TOW013', 'TOW254', 'TOW_VOL07',
 #                  'TOW_VOL09', 'TOW132', 'TOW125', 'TOW049', 'TOW_VOL10', 'TOW259', 'TOW080', 'TOW257', 'TOW042', 'TOW_VOL002', 'TOW113', 'TOW097', 'TOW040', 
 #                  'TOW251', 'TOW046', 'TOW_VOL05', 'TOW201', 'TOW_VOL06', 'TOW017', 'TOW011', 'TOW018']
 # pattern = re.compile(r'TOW(?:_VOL)?(\d+)_')
-# for folder in finalfolders:
-#     for file in os.listdir(path+"/"+folder):
-#         if "rec-mag" in file:
-#             file4d = file
-#             img = nib.load(path+"/"+folder+"/"+file4d).get_fdata()
-#             img = img[:,:,:,::2]
-#             img_arr = np.array(img)
-#             norm_img_arr = 255 * (img_arr - np.min(img_arr)) / (np.max(img_arr) - np.min(img_arr))
-#             norm_img_arr = norm_img_arr.astype(np.uint8)
-#             img = norm_img_arr
-#             # img = img[:,:,:,0]
-# #           print(file4d)
-# #           print(img.shape)
-# #           print(type(resized_img_2))
-#             match = pattern.search(file4d)
-#             for i in range(img.shape[2]):
-#                 for j in range(img.shape[3]):
-#                     slice_2d = img[:,:,i,j]
-#                     image = Image.fromarray(slice_2d)
-#                     # print(image.mode)
 
-#                     if image.mode != 'RGB':
-#                         image = image.convert('RGB')
-#                     filename = f'img_{match.group(0)}_slice_{i+1}_{j+1}.jpg'
-#                     file_path = os.path.join(outputdir, filename)
-#                     print(file4d, slice_2d.shape, i, j, filename)
-#                     image.save(file_path, 'JPEG')
+for folder in finalfolders:
+    for file in os.listdir(path+"/"+folder):
+        if "rec-mag" or "res-mag" in file:
+            file4d = file
+            img = nib.load(path+"/"+folder+"/"+file4d).get_fdata()
+            img = img[:,:,:,::2]
+            img_arr = np.array(img)
+            print(img_arr.shape)
+            ##if img.shape == (256, 256, 70, 30):
+#                 ## print(img.shape)
+            # norm_img_arr = 255 * (img_arr - np.min(img_arr)) / (np.max(img_arr) - np.min(img_arr))
+            # norm_img_arr = norm_img_arr.astype(np.uint8)
+            # img = norm_img_arr
+            # img = img[:,:,:,0]
+#           print(file4d)
+#           print(img.shape)
+#           print(type(resized_img_2))
+            # match = pattern.search(file4d)
+            # for i in range(img.shape[2]):
+            #     for j in range(img.shape[3]):
+            #         slice_2d = img[:,:,i,j]
+            #         image = Image.fromarray(slice_2d)
+            #         # print(image.mode)
+
+            #         if image.mode != 'RGB':
+            #             image = image.convert('RGB')
+            #         filename = f'img_{match.group(0)}_slice_{i+1}_{j+1}.jpg'
+            #         file_path = os.path.join(outputdir, filename)
+            #         print(file4d, slice_2d.shape, i, j, filename)
+            #         image.save(file_path, 'JPEG')
 
 
 
