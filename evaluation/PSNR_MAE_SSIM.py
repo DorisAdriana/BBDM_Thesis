6928895569a65590bd0ef6c59aaa7e9118ef3cd6
DorisAdriana <doriswezenberg@live.nl>
2024-05-17 14:44:46 +0200

    a

--- evaluation/PSNR_MAE_SSIM.py
+++ evaluation/PSNR_MAE_SSIM.py
@@ -92,7 +92,9 @@ def evaluate_metrics(ground_truth_dir, generated_dir):
     return subject_averages, subject_stddevs
 
 if __name__ == "__main__":
-    ground_truth_dir = 'results/BBDM_n98_s256x256_e10/BrownianBridge/samples_to_eval/ground_truth'
-    generated_dir = 'results/BBDM_n98_s256x256_e10/BrownianBridge/samples_to_eval/200'
+    ground_truth_dir = 'results/BBDM_n98_s256x256_z88_e10/BrownianBridge/sample_to_eval/ground_truth'
+    generated_dir = 'results/BBDM_n98_s256x256_z88_e10/BrownianBridge/sample_to_eval/200'
 
     evaluate_metrics(ground_truth_dir, generated_dir)
+
+
