e34593b2cee044a60381595ed676e2fb2e214ce7
DorisAdriana <doriswezenberg@live.nl>
2024-07-24 11:29:31 +0200

    segs and metrics

--- pred_slices_to_nii_UNET.py
+++ pred_slices_to_nii_UNET.py
@@ -24,27 +24,15 @@ def create_nifti_from_slices(num_scans, num_slices_a, num_slices_b, scan_ids, ou
             slice_a_images = []
             for a in range(1, num_slices_a + 1):
                 if data_type == 'pred':
-                    folder_name = f"img_TOW{scan_id}__slice_{a}_{b}"
-                    folder_path = os.path.join(input_directory, folder_name)
-                    file_list = sorted(os.listdir(folder_path))
-                elif data_type == 'gt':
-                    folder_name = f"img_TOW{scan_id}__slice_{a}_{b}.jpg"  
-                    folder_path = input_directory
-                    file_list = [folder_name]
-
-                found_image = False
-                for file in file_list:
-                    if file.endswith(".png"): # jpg for gt, png for pred 
-                        img_path = os.path.join(folder_path, file) if data_type == 'pred' else os.path.join(folder_path, folder_name)
-                        img = Image.open(img_path)
-                        img = img.resize((256, 256), Image.Resampling.LANCZOS)
-                        img_array = np.array(img)
-                        #print(img_array.shape)
-                        slice_a_images.append(img_array)
-                        found_image = True
-                        break
-                if not found_image:
-                    print(f"No suitable image found in {folder_path}.")
+                    file_name = f"img_TOW{scan_id}__slice_{a}_{b}.jpg"
+                    file_path = os.path.join(input_directory, file_name)
+                    img = Image.open(file_path)
+                    img = img.resize((256, 256), Image.Resampling.LANCZOS)
+                    img_array = np.array(img)
+                    #print(img_array.shape)
+                    slice_a_images.append(img_array)
+                    found_image = True
+                    break
             
             if slice_a_images:
                 # For each 'b', create a 3D array (stacking along the third dimension, axis=2)
@@ -63,11 +51,11 @@ def create_nifti_from_slices(num_scans, num_slices_a, num_slices_b, scan_ids, ou
                 print(f"Warning: No images were found for 'b'={b} in scan {scan_id}. Skipping this 'b' value.")
 
 create_nifti_from_slices(
-    num_scans=8, 
+    num_scans=20, 
     num_slices_a=88, 
     num_slices_b=15, 
-    scan_ids=['143', '144', '146', '201', '213', '216','218', '224'], # 239 not finished yet and no volunteers included yet
-    output_directory="my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/BBDM_Thesis/Unet_input/pred_niftis", 
-    input_directory= 'my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/BBDM_Thesis/Unetbaseline_dropout' #'/home/rnga/dawezenberg/my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/BBDM_Thesis/results/BBDM_n98_s256x256_z88_e10/BrownianBridge/sample_to_eval/200/',#"/home/rnga/dawezenberg/my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/data/processed/bSSFP_slices_n98_s320x320_z88", #'/home/rnga/dawezenberg/my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/BBDM_Thesis/results/BBDM_60e_98s_256/BrownianBridge/sample_to_eval/200/', # for gt: "/home/rnga/dawezenberg/my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/data/processed/bSSFP_slices_n98_s320x320_z88",
+    scan_ids= ['_VOL12', '_VOL13', '_VOL14', '_VOL15', '143', '144','146', '201','213', '216', '218', '224','239', '246', '247', '248', '251', '254', '257', '259'],# ['143', '144', '146', '201', '213', '216','218', '224'],
+    output_directory="my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/BBDM_Thesis/segmentations/inputs/Unet/pred_niftis", 
+    input_directory= 'my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/BBDM_Thesis/Unetbaseline_dropout/results/predictions/10_epoch', #/home/rnga/dawezenberg/my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/BBDM_Thesis/results/BBDM_n98_s256x256_z88_e10/BrownianBridge/sample_to_eval/200/',#"/home/rnga/dawezenberg/my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/data/processed/bSSFP_slices_n98_s320x320_z88", #'/home/rnga/dawezenberg/my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/BBDM_Thesis/results/BBDM_60e_98s_256/BrownianBridge/sample_to_eval/200/', # for gt: "/home/rnga/dawezenberg/my-rdisk/r-divi/RNG/Projects/stages/Pim/Doris/data/processed/bSSFP_slices_n98_s320x320_z88",
     data_type= 'pred'  # 'gt' or 'pred'
 )
